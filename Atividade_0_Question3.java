c09203c23183a5ed1b6072c974718fd049bde9a3
Thiago Araujo <161383143+Thiago-Arauj@users.noreply.github.com>
2024-08-14 16:37:56 -0300

    Loop com while

---
+++ Atividade_0_Question3.java
@@ -0,0 +1,16 @@
+public class Atividade_0_Question3 {
+
+    public static void main(String[] args){
+        int num = 10;
+        int sum = 0;
+
+        while (num > 0){
+            System.out.println(num);
+            sum += num;
+            num--;
+        }
+
+        System.out.println("Terminamos a contagem, o resultado da soma é: " + sum);
+
+    }
+}
