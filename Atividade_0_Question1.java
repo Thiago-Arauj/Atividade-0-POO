f14f7a80632b33d366f6b13618cb9693ccf1ab86
Thiago Araujo <161383143+Thiago-Arauj@users.noreply.github.com>
2024-08-14 16:36:27 -0300

    Operações numéricas

---
+++ Atividade_0_Question1.java
@@ -0,0 +1,20 @@
+public class Atividade_0_Question1 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 30;
+
+        // Transformando as entradas inteiras em double para que a divisão exata seja sempre possível
+        double c = a;
+        double d = b;
+
+
+        int product = a * b;
+        double division = c / d;
+        int addition = a + b;
+        int subtraction = a - b;
+
+        System.out.println("Soma = " + addition + "\nSubtração = " + subtraction +
+                "\nProduto = " + product + "\nDivisão = " + division);
+
+    }
+}
